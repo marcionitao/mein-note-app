5f9a07014ccdae8e2224c680bed4ed79aebf8240
Marcio Nitao <marcionitao@gmail.com>
2021-06-24 16:14:19 +0200

    config NoteCard and Container component

---
+++ src/components/NoteCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-card flat bordered class="q-mb-sm cursor-pointer" @click="$emit('click', $event)">
+    <q-card-section class="q-pt-xs">
+      <div class="text-h5 q-mt-sm q-mb-xs">
+        {{ title }}
+      </div>
+      <div class="text-caption text-grey">
+        {{ decription }}
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    decription: String
+  },
+  setup () {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-
-  </div>
+  <q-page padding>
+    <Container>
+      <div class="row items-center justify-between">
+        <h3>Your notes</h3>
+        <div>
+          <q-btn round color="positive" icon="add" to="/new" />
+        </div>
+      </div>
+      <NoteCard
+        v-for="({ title, description }, idx) in notes"
+        :key="idx"
+        :title="title"
+        :description="description"
+        @click="router.push(`/note/${idx}`)">
+      </NoteCard>
+      <div v-if="notes.length === 0">You have not created any notes.</div>
+    </Container>
+  </q-page>
 </template>
 
 <script>
 import Container from './../components/Container.vue'
 import NoteCard from './../components/NoteCard.vue'
-import { useLocalStorage } from './../helper'
+import { useLocalNotes } from './../helper'
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router'
 
@@ -20,9 +35,9 @@ export default defineComponent({
     NoteCard
   },
   setup() {
-
-    return { Container, NoteCard}
+    const notes = useLocalNotes();
+    const router = useRouter();
+    return { Container, NoteCard, notes, router}
   }
 })
 </script>
-
